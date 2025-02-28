6e071c3bdef1cd4aa50ac99690ee2c903aca6594
vishalgiri2612 <er.vishalgiri2612@gamil.com>
2025-02-28 18:21:18 +0530

    change in database

--- back-end/dbConnection.js
+++ back-end/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async() => {
   try{
-    await mongoose.connect('mongodb://127.0.0.1:27017/bookMyShow');
+    await mongoose.connect('mongodb+srv://Vishal:1234567890@cluster0.dyypa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
     console.log('Connected to Database')}
   catch(error){
     console.log(error)}
